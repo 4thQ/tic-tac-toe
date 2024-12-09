dab05ebbfc07eb727b303ff330af1f4da8ec7ca5
Nirajan Gurung <diorrg@outlook.com>
2024-12-09 20:10:10 +0000

    winner ux updated

--- ttt.js
+++ ttt.js
@@ -9,6 +9,32 @@
 //   return { getValue, addValue };
 // }
 
+// const array = [
+//   [4, 2, 3],
+//   [4, 4, 4],
+//   [4, 8, 9],
+// ];
+// //row
+// for (const row of array) {
+//   console.log(row);
+//   let check = row.every((i) => i == 4);
+//   console.log(check);
+//   if (check == true) {
+//     console.log("Row winner");
+//   }
+// }
+
+// //col
+// for (let i = 0; i < 3; i++) {
+//   const column = array.map((row) => row[i]); //
+
+//   columnWinner = column.every((c) => c == 4);
+
+//   if (columnWinner == true) {
+//     console.log("Col Winner!");
+//   }
+// }
+
 //Game board
 function gameBoard() {
   const rows = 3;
@@ -62,11 +88,42 @@ function gameController() {
   }
 
   function checkWinner() {
-    const isWinner = game.board.some((row) => row.every((col) => col === "1"));
-    if (isWinner === true) {
-      console.log("Winner");
-    } else {
-      console.log("asd");
+    //row
+    let gameArray = game.board;
+
+    for (const row of gameArray) {
+      let check = row.every((i) => i == 1);
+      if (check == true) {
+        console.log("Row winner1");
+      }
+    }
+
+    for (const row of gameArray) {
+      let check2 = row.every((i) => i == 2);
+      if (check2 == true) {
+        console.log("Row winner2");
+      }
+    }
+
+    //col
+    for (let i = 0; i < 3; i++) {
+      const column = gameArray.map((row) => row[i]); //
+
+      columnWinner = column.every((c) => c == 1);
+
+      if (columnWinner == true) {
+        console.log("Col Winner!");
+      }
+    }
+
+    for (let i = 0; i < 3; i++) {
+      const column = gameArray.map((row) => row[i]); //
+
+      columnWinner2 = column.every((c) => c == 2);
+
+      if (columnWinner2 == true) {
+        console.log("Col Winner2!");
+      }
     }
   }
 
@@ -75,8 +132,8 @@ function gameController() {
 
 const g1 = gameController();
 console.log(g1.playRound(1, 0));
-console.log(g1.playRound(1, 2));
+console.log(g1.playRound(0, 0));
 console.log(g1.playRound(1, 1));
-console.log(g1.playRound(0, 2));
+console.log(g1.playRound(1, 0));
 console.log(g1.playRound(1, 2));
-console.log(g1.checkWinner());
+console.log(g1.playRound(2, 0));
